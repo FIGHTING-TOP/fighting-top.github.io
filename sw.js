503660646e337caa4eb542078175143b2d922e7b
WangMingwei <wangmingwei623@163.com>
2020-10-20 15:33:05 +0800

    Update blog

--- sw.js
+++ sw.js
@@ -24,15 +24,15 @@ var precacheUrls = [];
 
   precacheUrls.push('/');
 
-  precacheUrls.push('/article/ornerstone/');
+  precacheUrls.push('/cornerstone/ornerstone/');
 
   precacheUrls.push('/Java/Java基础/Java基础之反射、注解/');
 
-  precacheUrls.push('/Java/Java基础/Java基础之File类、递归/');
+  precacheUrls.push('/Java/Java基础之File类、递归/');
 
   precacheUrls.push('/Java/Java基础/Java基础之字节流、字符流/');
 
-  precacheUrls.push('/Java/Java基础/网络编程/Java基础之网络编程/');
+  precacheUrls.push('/Java/Java基础/Java基础之缓冲流、转换流、序列化流、打印流/');
 
 toolbox.precache(precacheUrls);
 toolbox.options = {"networkTimeoutSeconds":5};
